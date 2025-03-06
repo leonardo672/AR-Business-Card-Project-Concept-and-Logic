91259158540d2ac9f51f36bbec524cc5caf02729
Homam AL Safadi (Хомам Ал сафади) <73609700+leonardo672@users.noreply.github.com>
2025-03-06 20:45:32 +0300

    Add files via upload

---
+++ Scripts/Link_Manager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Link_Manager : MonoBehaviour
+{
+   public void openYouTube()
+    {
+        Application.OpenURL("https://www.youtube.com/watch?v=WzfDo2Wpxks&t=30786s");
+    }
+}
